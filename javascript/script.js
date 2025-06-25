eb7b423e328633d460663fcb9922f766b7a257c7
Manuella Kuiawa <manuelladefatima166@gmail.com>
2025-06-25 10:32:56 -0300

    Ajustando o estilo do header responsivo

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('#mobile_btn').on('click', function() {
         $('#mobile_menu').toggleClass('active');
-        $('#mobile_btn').find('i').toggleClass('fa-x');
+        $(this).find('i').toggleClass('fa-bars fa-x');
     });
-})
+});
