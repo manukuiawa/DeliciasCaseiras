30ee9f73b2d53b5bb46a4f38356476a87ad78578
Manuella Kuiawa <manuelladefatima166@gmail.com>
2025-06-28 23:05:13 -0300

    Criando o efeito slide para menu mobile

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,22 @@
-$(document).ready(function() {
-    $('#mobile_btn').on('click', function() {
-        $('#mobile_menu').toggleClass('active');
-        $(this).find('i').toggleClass('fa-bars fa-x');
-    });
+$(document).ready(function () {
+  let $menu = $('#mobile_menu');
+  let $icon = $('#mobile_btn i');
+
+  $('#mobile_btn').on('click', function () {
+    if (!$menu.hasClass('show')) {
+      $menu.removeClass('hide').addClass('show').css('display', 'flex');
+      $icon.removeClass('fa-bars').addClass('fa-x');
+    } else {
+      $menu.removeClass('show').addClass('hide');
+      $icon.removeClass('fa-x').addClass('fa-bars');
+
+      // Espera a animação terminar pra esconder de vez
+      setTimeout(() => {
+        $menu.css('display', 'none');
+      }, 400); // igual ao tempo do slideUp
+    }
+  });
 });
+
+
+
